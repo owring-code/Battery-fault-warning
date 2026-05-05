9d05306720ad552c421c42c71147caa71ec14bc8
OW <133517516+owring-code@users.noreply.github.com>
2026-05-05 11:53:10 +0800

    Delete code directory

--- code/code/scripts/plotting/plot_style.py
+++
@@ -1,5 +0,0 @@
-﻿from battery_thesis.plot_style import apply_academic_plot_style
-
-
-if __name__ == '__main__':
-    apply_academic_plot_style()
--- code/code/tests/test_build_samples_contract.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-from scripts.build_samples import resolve_label_path
-from battery_thesis.config import FINAL_LABELS_ROOT
-
-
-def test_build_samples_reads_labels_from_final_root():
-    path = resolve_label_path('WCVT1000000000099', 'structured_dataset')
-    assert path == FINAL_LABELS_ROOT / 'per_vehicle' / 'WCVT1000000000099_labels.csv'
